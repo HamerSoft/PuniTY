721cabf9c16de5d56743f76ed450324447059c20
Ruben Hamers <ruben_hamers@hotmail.com>
2024-04-19 07:06:21 +0200

    chore: add move and scroll command

---
+++ Runtime/AnsiEncoding/Sequences/CursorSequences/MoveAndScrollUp.cs
@@ -0,0 +1,12 @@
+﻿namespace HamerSoft.PuniTY.AnsiEncoding
+{
+    public class MoveAndScrollUp : Sequence
+    {
+        public override char Command => 'M';
+
+        public override void Execute(IScreen screen, string parameters)
+        {
+            screen.SetCursorPosition(new Position(screen.Cursor.Position.Row - 1, screen.Cursor.Position.Column));
+        }
+    }
+}
---
+++ Tests/Editor/AnsiDecoding/CSISequenceTests/MoveAndScrollTests.cs
@@ -0,0 +1,27 @@
+﻿using HamerSoft.PuniTY.AnsiEncoding;
+using NUnit.Framework;
+
+namespace HamerSoft.PuniTY.Tests.Editor.AnsiDecoding.CSISequenceTests
+{
+    [TestFixture]
+    public class MoveAndScrollTests : AnsiDecoderTest
+    {
+        [SetUp]
+        public override void SetUp()
+        {
+            base.SetUp();
+            Screen = new MockScreen(10, 10);
+            AnsiDecoder = new AnsiDecoder(Screen,
+                EscapeCharacterDecoder,
+                new MoveAndScrollUp());
+        }
+
+        [Test]
+        public void When_Cursor_MoveAndScroll_CursorMoves_1_Line_Up()
+        {
+            Screen.SetCursorPosition(new Position(2, 2));
+            Decode($"\x001bM");
+            Assert.That(Screen.Cursor.Position, Is.EqualTo(new Position(1, 2)));
+        }
+    }
+}
