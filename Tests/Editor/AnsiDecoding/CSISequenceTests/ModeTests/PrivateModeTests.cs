24af577435166852bbd7ace2943aaadfe8e9a880
Ruben Hamers <ruben_hamers@hotmail.com>
2024-09-05 06:41:15 +0200

    chore: refactor tests and add modetests

--- Tests/Editor/AnsiDecoding/CSISequenceTests/ModeTests/PrivateModeTests.cs
+++ Tests/Editor/AnsiDecoding/CSISequenceTests/ModeTests/PrivateModeTests.cs
@@ -20,150 +20,37 @@ namespace HamerSoft.PuniTY.Tests.Editor.AnsiDecoding.CSISequenceTests
                     typeof(ResetModeSequence)));
         }
 
-        [Test]
-        public void Private_SetMode_1h_Sets_ApplicationCursorKeys_Mode()
+        [TestCase(1, AnsiMode.ApplicationCursorKeys)]
+        [TestCase(2, AnsiMode.DECANM)]
+        [TestCase(3, AnsiMode.DECCOLM)]
+        [TestCase(4, AnsiMode.SmoothScroll)]
+        [TestCase(5, AnsiMode.ReverseVideo)]
+        [TestCase(6, AnsiMode.Origin)]
+        [TestCase(7, AnsiMode.AutoWrap)]
+        [TestCase(8, AnsiMode.AutoRepeatKeys)]
+        [TestCase(9, AnsiMode.SendMouseXY)]
+        public void Private_SetMode_Sets_Correct_AnsiMode(int command, AnsiMode expectedMode)
         {
-            SetMode(1);
-            Assert.That(Screen.HasMode(AnsiMode.ApplicationCursorKeys), Is.True);
+            SetMode(command);
+            Assert.That(Screen.HasMode(expectedMode), Is.True);
         }
 
-        [Test]
-        public void Private_ResetMode_1l_Resets_ApplicationCursorKeys_Mode()
+        [TestCase(1, AnsiMode.ApplicationCursorKeys)]
+        [TestCase(2, AnsiMode.DECANM)]
+        [TestCase(3, AnsiMode.DECCOLM)]
+        [TestCase(4, AnsiMode.SmoothScroll)]
+        [TestCase(5, AnsiMode.ReverseVideo)]
+        [TestCase(6, AnsiMode.Origin)]
+        [TestCase(7, AnsiMode.AutoWrap)]
+        [TestCase(8, AnsiMode.AutoRepeatKeys)]
+        [TestCase(9, AnsiMode.SendMouseXY)]
+        public void Private_ResetMode_Resets_Correct_AnsiMode(int command, AnsiMode expectedMode)
         {
-            SetMode(1);
-            Assert.That(Screen.HasMode(AnsiMode.ApplicationCursorKeys), Is.True);
-            ResetMode(1);
-            Assert.That(Screen.HasMode(AnsiMode.ApplicationCursorKeys), Is.False);
+            SetMode(command);
+            Assert.That(Screen.HasMode(expectedMode), Is.True);
+            ResetMode(command);
+            Assert.That(Screen.HasMode(expectedMode), Is.False);
         }
-
-        [Test]
-        public void Private_SetMode_2h_Sets_DECANM_Mode()
-        {
-            SetMode(2);
-            Assert.That(Screen.HasMode(AnsiMode.DECANM), Is.True);
-        }
-
-        [Test]
-        public void Private_ResetMode_2l_Resets_DECANM_Mode()
-        {
-            SetMode(2);
-            Assert.That(Screen.HasMode(AnsiMode.DECANM), Is.True);
-            ResetMode(2);
-            Assert.That(Screen.HasMode(AnsiMode.DECANM), Is.False);
-        }
-
-        [Test]
-        public void Private_SetMode_3h_Sets_DECCOLM_Mode()
-        {
-            SetMode(3);
-            Assert.That(Screen.HasMode(AnsiMode.DECCOLM), Is.True);
-        }
-
-        [Test]
-        public void Private_ResetMode_3l_Resets_DECCOLM_Mode()
-        {
-            SetMode(3);
-            Assert.That(Screen.HasMode(AnsiMode.DECCOLM), Is.True);
-            ResetMode(3);
-            Assert.That(Screen.HasMode(AnsiMode.DECCOLM), Is.False);
-        }
-
-        [Test]
-        public void Private_SetMode_4h_Sets_SmoothScroll_Mode()
-        {
-            SetMode(4);
-            Assert.That(Screen.HasMode(AnsiMode.SmoothScroll), Is.True);
-        }
-
-        [Test]
-        public void Private_ResetMode_4l_Resets_SmoothScroll_Mode()
-        {
-            SetMode(4);
-            Assert.That(Screen.HasMode(AnsiMode.SmoothScroll), Is.True);
-            ResetMode(4);
-            Assert.That(Screen.HasMode(AnsiMode.SmoothScroll), Is.False);
-        }
-
-        [Test]
-        public void Private_SetMode_5h_Sets_ReverseVideo_Mode()
-        {
-            SetMode(5);
-            Assert.That(Screen.HasMode(AnsiMode.ReverseVideo), Is.True);
-        }
-
-        [Test]
-        public void Private_ResetMode_5l_Resets_ReverseVideo_Mode()
-        {
-            SetMode(5);
-            Assert.That(Screen.HasMode(AnsiMode.ReverseVideo), Is.True);
-            ResetMode(5);
-            Assert.That(Screen.HasMode(AnsiMode.ReverseVideo), Is.False);
-        }
-
-        [Test]
-        public void Private_SetMode_6h_Sets_Origin_Mode()
-        {
-            SetMode(6);
-            Assert.That(Screen.HasMode(AnsiMode.Origin), Is.True);
-        }
-
-        [Test]
-        public void Private_ResetMode_6l_Resets_Origin_Mode()
-        {
-            SetMode(6);
-            Assert.That(Screen.HasMode(AnsiMode.Origin), Is.True);
-            ResetMode(6);
-            Assert.That(Screen.HasMode(AnsiMode.Origin), Is.False);
-        }
-
-        [Test]
-        public void Private_SetMode_7h_Sets_Origin_Mode()
-        {
-            SetMode(7);
-            Assert.That(Screen.HasMode(AnsiMode.AutoWrap), Is.True);
-        }
-
-        [Test]
-        public void Private_ResetMode_7l_Resets_Origin_Mode()
-        {
-            SetMode(7);
-            Assert.That(Screen.HasMode(AnsiMode.AutoWrap), Is.True);
-            ResetMode(7);
-            Assert.That(Screen.HasMode(AnsiMode.AutoWrap), Is.False);
-        }
-
-        [Test]
-        public void Private_SetMode_8h_Sets_Origin_Mode()
-        {
-            SetMode(8);
-            Assert.That(Screen.HasMode(AnsiMode.AutoRepeatKeys), Is.True);
-        }
-
-        [Test]
-        public void Private_ResetMode_8l_Resets_Origin_Mode()
-        {
-            SetMode(8);
-            Assert.That(Screen.HasMode(AnsiMode.AutoRepeatKeys), Is.True);
-            ResetMode(8);
-            Assert.That(Screen.HasMode(AnsiMode.AutoRepeatKeys), Is.False);
-        }
-
-        [Test]
-        public void Private_SetMode_9h_Sets_Origin_Mode()
-        {
-            SetMode(9);
-            Assert.That(Screen.HasMode(AnsiMode.SendMouseXY), Is.True);
-        }
-
-        [Test]
-        public void Private_ResetMode_9l_Resets_Origin_Mode()
-        {
-            SetMode(9);
-            Assert.That(Screen.HasMode(AnsiMode.SendMouseXY), Is.True);
-            ResetMode(9);
-            Assert.That(Screen.HasMode(AnsiMode.SendMouseXY), Is.False);
-        }
-
 
         private void SetMode(int command)
         {
---
+++ Tests/Editor/AnsiDecoding/ModeTests.cs
@@ -0,0 +1,95 @@
+﻿using HamerSoft.PuniTY.AnsiEncoding;
+using NUnit.Framework;
+
+namespace HamerSoft.PuniTY.Tests.Editor.AnsiDecoding
+{
+    [TestFixture]
+    public class ModeTests : AnsiDecoderTest
+    {
+        [SetUp]
+        public override void SetUp()
+        {
+            base.SetUp();
+            Screen = new MockScreen(2, 2);
+        }
+
+        [Test]
+        public void IMode_By_Default_Is_None()
+        {
+            Assert.That(Screen.Mode, Is.EqualTo(AnsiMode.None));
+        }
+
+        [Test]
+        public void IMode_SetMote_Removes_Mode_None()
+        {
+            Screen.SetMode(AnsiMode.Origin);
+            Assert.That(Screen.Mode, Is.EqualTo(AnsiMode.Origin));
+            Assert.That(Screen.Mode, Is.Not.EqualTo(AnsiMode.None));
+        }
+
+        [Test]
+        public void IMode_SetMote_Enables_Mode()
+        {
+            Screen.SetMode(AnsiMode.Origin);
+            Assert.That(Screen.Mode, Is.EqualTo(AnsiMode.Origin));
+        }
+
+        [Test]
+        public void IMode_SetMode_Can_Enable_Multiple_Modes()
+        {
+            Screen.SetMode(AnsiMode.Origin);
+            Screen.SetMode(AnsiMode.SendReceive);
+            Screen.SetMode(AnsiMode.ReverseVideo);
+            Assert.That(Screen.Mode, Is.EqualTo(AnsiMode.Origin | AnsiMode.SendReceive | AnsiMode.ReverseVideo));
+        }
+
+        [Test]
+        public void IMode_HasMode_Returns_False_If_Mode_Not_Set()
+        {
+            Assert.That(Screen.HasMode(AnsiMode.Origin), Is.False);
+        }
+
+        [Test]
+        public void IMode_HasMode_Returns_True_If_Mode_Set()
+        {
+            Screen.SetMode(AnsiMode.Origin);
+            Assert.That(Screen.HasMode(AnsiMode.Origin), Is.True);
+        }
+
+        [Test]
+        public void IMode_HasMode_Returns_True_If_MultipleModes_Mode_Set()
+        {
+            Screen.SetMode(AnsiMode.Origin);
+            Screen.SetMode(AnsiMode.ReverseVideo);
+            Assert.That(Screen.HasMode(AnsiMode.Origin), Is.True);
+            Assert.That(Screen.HasMode(AnsiMode.ReverseVideo), Is.True);
+        }
+
+        [Test]
+        public void IMode_Reset_Mode_DoesNotThrow_If_Mode_Not_Set()
+        {
+            Assert.DoesNotThrow(() =>
+            {
+                Screen.ResetMode(AnsiMode.Origin);
+                Assert.That(Screen.Mode, Is.EqualTo(AnsiMode.None));
+            });
+        }
+
+        [Test]
+        public void IMode_Reset_Mode_Removes_Mode()
+        {
+            Screen.SetMode(AnsiMode.Origin);
+            Screen.SetMode(AnsiMode.ReverseVideo);
+            Assert.That(Screen.Mode, Is.EqualTo(AnsiMode.Origin | AnsiMode.ReverseVideo));
+            
+            Screen.ResetMode(AnsiMode.ReverseVideo);
+            Assert.That(Screen.HasMode(AnsiMode.Origin), Is.True);
+            Assert.That(Screen.HasMode(AnsiMode.ReverseVideo), Is.False);
+            
+            Screen.ResetMode(AnsiMode.Origin);
+            Assert.That(Screen.HasMode(AnsiMode.Origin), Is.False);
+            Assert.That(Screen.HasMode(AnsiMode.ReverseVideo), Is.False);
+            Assert.That(Screen.HasMode(AnsiMode.None), Is.True);
+        }
+    }
+}
